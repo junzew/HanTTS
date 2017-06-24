4892efd93a4dd670547f09474a4160bb85cae5a7
Junze Wu <junzew@hotmail.com>
2017-06-24 12:29:35 -0700

    Integrate chatbot API

--- main.py
+++ main.py
@@ -13,11 +13,12 @@ import _thread
 import time
 import sys
 import os
+import requests
 
 class TextToSpeech:
 
     CHUNK = 1024
-    punctuation = ['，', '。','？','！','“','”','；','：','（',"）"]
+    punctuation = ['，', '。','？','！','“','”','；','：','（',"）",":",";",",",".","?","!","\"","\'","(",")"]
 
     def __init__(self):
         pass
@@ -27,18 +28,37 @@ class TextToSpeech:
         syllables = lazy_pinyin(text, style=pypinyin.TONE2)
         print(syllables)
         delay = 0
+        
+        def preprocess(syllables):
+            temp = []
+            for syllable in syllables:
+                for p in TextToSpeech.punctuation:
+                    syllable = syllable.replace(p, "")
+                if syllable.isdigit():
+                    syllable = atc.num2chinese(syllable)
+                    new_sounds = lazy_pinyin(syllable, style=pypinyin.TONE2)
+                    for e in new_sounds:
+                        temp.append(e)
+                else:
+                    temp.append(syllable)
+            return temp
+
+        syllables = preprocess(syllables)
         for syllable in syllables:
+            
             sound = TextToSpeech.format_syll(syllable)
             path = "syllables/"+sound+".wav"
-            print(path)
+            # print(path)
             _thread.start_new_thread(TextToSpeech._play_audio, (path, delay))
             # TextToSpeech._play_audio(TextToSpeech.format_syll(syllable), delay)
             delay += 0.355
 
-    def synthesize(self, text):
+    def synthesize(self, text, src="./syllables/", dst="./audio/"):
         """
         Synthesize .wav from text
         Directory is relative to server/app.js
+        src is the folder that contains all syllables
+        dst is the destination folder to save the synthesized file
         """
         print("Synthesizing ...")
         delay = 0
@@ -50,7 +70,7 @@ class TextToSpeech:
         result = AudioSegment.silent(duration=500*len(text))
         for syllable in syllables:
             sound = TextToSpeech.format_syll(syllable)
-            path = "../syllables/"+sound+".wav"
+            path = src+sound+".wav"
             sound_file = Path(path)
             # insert 500 ms silence for punctuation marks
             if sound in TextToSpeech.punctuation:
@@ -65,7 +85,7 @@ class TextToSpeech:
             result = result.overlay(segment, position=delay)
             delay += increment
 
-        directory = "./audio/"
+        directory = dst
         if not os.path.exists(directory):
             os.makedirs(directory)
 
@@ -106,9 +126,37 @@ class TextToSpeech:
         except:
             pass
 
+    
+import atc
+APIkey = "64c88489ad7f432591d702ec1334dedc";
 if __name__ == '__main__':
     tts = TextToSpeech()
-    # while True:
-    #     tts.speak(input('输入中文：'))
-    text = sys.argv[1:][0]
-    tts.synthesize(text)
+    if len(sys.argv[1:]) == 0:
+        while True:
+            tts.speak(input('输入中文：'))
+    else:
+        option = sys.argv[1:][0]
+        if option == '-s': # synthesize    
+            text = sys.argv[1:][1]
+            tts.synthesize(text)
+        elif option == '-c': # chat
+            print("Hello there!")
+            while True:
+                sentence = input('输入中文：')
+                r = requests.post(
+                    "http://www.tuling123.com/openapi/api", 
+                    json = {
+                    "key": APIkey,
+                    "info": sentence, 
+                    "loc":"北京市中关村", 
+                    "userid":"1"
+                    })
+                response = r.json()["text"]
+                print(response)
+                tts.speak(response)
+        else:
+            print("usage: python main.py [-sc] {text}")
+            print("                       -s synthesize")
+            print("                       -c chat")
+
+
